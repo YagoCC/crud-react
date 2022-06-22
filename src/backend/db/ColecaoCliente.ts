14e274c810d6e9e1d50b1638abcf34aa455521b2
Yago Cordeiro <yago_alan@hotmail.com>
2022-06-22 00:44:36 -0300

    Colecao cliente firebase

---
+++ src/backend/db/ColecaoCliente.ts
@@ -0,0 +1,35 @@
+import Cliente from "../../../core/Cliente";
+import firebase from "../config";
+import ClienteRepositorio from "../../../core/ClienteRepositorio";
+
+export default class ColecaoCliente implements ClienteRepositorio {
+
+    #conversor = {
+        toFirestore(cliente: Cliente) {
+            return {
+                nome: cliente.nome,
+                idade: cliente.idade
+            }
+        },
+        fromFirestore(snapshot: firebase.firestore.QueryDocumentSnapshot, options: firebase.firestore.SnapshotOptions): Cliente {
+            const dados = snapshot.data(options)
+            return new Cliente(dados.nome, dados.idade, snapshot.id)
+        }
+
+    }
+
+    async salvar(cliente: Cliente): Promise<Cliente> {
+        return null
+    }
+    async excluir(cliente: Cliente): Promise<void> {
+        return this.colecao().doc(cliente.id).delete()
+    }
+    async obterTodos(): Promise<Cliente[]> {
+        return null
+    }
+
+    private colecao() {
+        return firebase.firestore().collection('clientes')
+        .withConverter(this.#conversor)
+    }
+}
